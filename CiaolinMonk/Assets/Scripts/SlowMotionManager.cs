ae0a0616ab422615b2752ee905224b401ad84795
Olivenders <32130024+Olivenders@users.noreply.github.com>
2019-02-23 13:51:20 +0100

    SlowMo
    SlowMo

---
+++ CiaolinMonk/Assets/Scripts/SlowMotionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotionManager : MonoBehaviour
+{
+    public float slowdownFactor = 0.05f;
+    public float slowdownLenght = 2f;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void DoSlowMotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    public void UndoSlowMotion()
+    {
+        Time.timeScale = 1f;
+    }
+
+   
+}
